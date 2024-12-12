31320170858cb7bb05752964a1df33339ba5e024
Saurabh Singh <saurabhsingh@Saurabhs-MacBook-Pro.local>
2024-12-12 14:47:48 +0530

    404 handler
    Signed-off-by: Saurabh Singh <saurabhsingh@Saurabhs-MacBook-Pro.local>

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { scrapeWebPage, chatWithGPT }  = require('./helpers');
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.get('/test', (req, res, next)=> res.send('Server Running'))
+app.get('/test', (req, res, next)=> res.status(200).send('Server Running'))
 app.post('/checkcompliance', async (req, res, next)=>{
     url = req.body.url
     if (!url) {
@@ -18,12 +18,14 @@ app.post('/checkcompliance', async (req, res, next)=>{
     try {
         const data = await scrapeWebPage(url);
         output = await chatWithGPT(data.join('\n'))
-        console.log(output)
-        res.json(output);
+        res.status(200).json(output);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 })
+app.use((req, res, next) => {
+    res.status(404).json({ error: '404 Not Found', message: 'The requested resource could not be found.'});
+});
 
 
 const port = process.env.PORT || 5001
